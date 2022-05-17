983faa629ab7d9fc57561b65fcbb1a12668e4b74
ruipaulocalei <ruipaulocalei0@gmail.com>
2022-05-17 05:24:10 +0100

    Instagram Icon

--- lib/ui/pages/loading_page.dart
+++ lib/ui/pages/loading_page.dart
@@ -13,22 +13,28 @@ class LoadingPage extends ConsumerWidget {
       body: FutureBuilder(
         future: loginViewModel.checkToken(context, ref),
         builder: (context, snapshot) {
-            return Center(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    FaIcon(FontAwesomeIcons.instagram)
-                    // LinearProgressIndicator(),
-                    // Text('FROM'),
-                    // Text('PALM RISE')
-                  ],
+          return Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(),
+                const FaIcon(
+                  FontAwesomeIcons.instagram,
+                  size: 50,
                 ),
-              ),
-            );
-        },),
+                // LinearProgressIndicator(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [Text('FROM'), Text('META')],
+                )
+              ],
+            ),
+          );
+        },
+      ),
     );
   }
 }
